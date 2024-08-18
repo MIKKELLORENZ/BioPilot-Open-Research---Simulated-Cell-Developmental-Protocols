fb823bdf0451f02f93c6bc785b8de4506bf9ce3e
mvlorenz <mikkelvind@gmail.com>
2024-08-18 21:55:25 +0000

    chore: Add functions for making database entries and update main.py

--- funcs/db_funcs.py
+++ funcs/db_funcs.py
@@ -1,11 +1,70 @@
 import os
+import psycopg2
+import hashlib
 
-def get_db_creds():
+# Function to make md5 hash
+def make_md5_hash(s):
+    return hashlib.md5(s.encode()).hexdigest()
 
-    db_d = {}
-    db_d['user'] = os.getenv('DB_USER')
-    db_d['password'] = os.getenv('DB_PW')
-    db_d['ip'] = os.getenv('DB_IP')
-    db_d['db'] = os.getenv('DB_NAME')
+# Function to get connection and cursor to database
+def get_conn_cur():
 
-    return db_d
+    # Get database credentials
+    db_cred = {}
+    db_cred['ip'] = os.getenv('DB_IP')
+    db_cred['db'] = os.getenv('DB_NAME')
+    db_cred['user'] = os.getenv('DB_USER')
+    db_cred['password'] = os.getenv('DB_PW')
+       
+
+    print(db_cred)
+    # Connect to database
+    conn = psycopg2.connect(
+        host=db_cred['ip'],
+        database=db_cred['db'],
+        user=db_cred['user'],
+        password=db_cred['password']
+    )
+    cur = conn.cursor()
+
+    return conn, cur
+
+# Function to make database entry for LLM response
+def make_run_db_entry(d):
+
+# Get database connection and cursor 
+    conn, cur = get_conn_cur()
+
+# Format keys,value and query for insertion
+    values = ', '.join(['%s'] * len(d))
+    keys = ', '.join(d.keys())
+    q = f"""INSERT INTO protol_gen_runs_overview ({keys},last_modified) VALUES ({values},now()) RETURNING pool_id"""
+   
+# Execute query
+    cur.execute(q, tuple(d.values()))
+    
+# Get pool_id
+    pool_id = cur.fetchone()[0]
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    return pool_id
+
+# Function to make database entry for LLM output
+def make_output_db_entry(project,output,pool_id,mdl):
+
+    # Get database connection and cursor 
+    conn,cur = get_conn_cur()
+
+    # Format query for insertion
+    q = f"""INSERT INTO {project} (model, output, pool_id,last_modified) VALUES (%s, %s, %s,now())"""
+    
+    # Execute query
+    cur.execute(q, (mdl,output,pool_id))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    return None
--- main.py
+++ main.py
@@ -1,5 +1,49 @@
 import os
-from funcs.db_funcs import get_db_creds 
+from ollama import Client
+from funcs.db_funcs import make_md5_hash
+from funcs.db_funcs import make_run_db_entry
+from funcs.db_funcs import make_output_db_entry
 
-db_cred = get_db_creds()
+# Set up the client
+client = Client(host='http://192.168.1.11:11434')
+
+# Read prompt from prompts folder
+prompt = open("prompts/liver_organoids_1.txt", "r").read()
+
+# Set model to use in generating response
+model = 'llama3.1:8b' # 8b model
+
+# number of protocols to generate
+no_protocols_per_model = 5
+
+# Which models to use
+models = ['llama3.1:8b', 'gemma2', 'mistral-nemo']
+
+# Set data for database entry
+d = {}
+d["project"] = "simulated_cell_development_protocols"
+d["prompt_md5_hash"] = make_md5_hash(prompt)
+d["product"] = "liver_organoids"
+
+# Make database entry for current run
+pool_id = make_run_db_entry(d)
+
+# Main loop to generate protocols
+for mdl in models:
+    for i in range(no_protocols_per_model):
+        print("Generating protocol", i+1, "for model", mdl)
+        response = client.chat(model=mdl, messages=[
+        {
+            'role': 'user',
+            'content': f'{prompt}',
+        },
+        ])
+
+# Get output from response
+        output = response["message"]["content"]
+
+
+# Make database entry for current output
+        make_output_db_entry(d["project"],output,pool_id,mdl)
+
 
